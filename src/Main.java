b1f0e362c08df2b3324ed6840f723e05b964326f
billygrosmollets <clementbilly04@gmail.com>
2024-10-02 10:56:47 +0200

    mog yassco

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//Main.java
+
+public class Main
+{
+    public static void main(String[] arg)
+    {
+        Pong pong = new Pong();
+        View view = new View(pong);
+        Controller controller = new Controller(pong, view);
+    }
+}
