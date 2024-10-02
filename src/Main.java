c106ed354e61b5c24dca7cbb4c5693dab5275b67
billygrosmollets <clementbilly04@gmail.com>
2024-10-02 11:17:27 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,6 @@ public class Main
         Pong pong = new Pong();
         View view = new View(pong);
         Controller controller = new Controller(pong, view);
+        System.out.println("TEST");
     }
 }
